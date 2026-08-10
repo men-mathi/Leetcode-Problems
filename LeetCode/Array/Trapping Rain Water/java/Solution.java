f47fb051f0c92ddcb13de7e364d5544714da1a1d
men-mathi <menmathianand2005@gmail.com>
2026-08-10 20:21:11 +0530

    feat: Add Trapping Rain Water

---
+++ LeetCode/Array/Trapping Rain Water/java/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int trap(int[] arr) {
+       int water=0;
+       int left=0;
+       int right=arr.length-1;
+       int leftmax=0;
+       int rightmax=0;
+       while(left<right){
+        if(arr[left]<arr[right]){
+            if(arr[left]>=leftmax){
+                leftmax=arr[left];
+            }
+            else{
+                water+=leftmax-arr[left];
+            }
+            left++;
+        }
+        else{
+            if(arr[right]>=rightmax){
+                rightmax=arr[right];
+            }
+            else{
+                water+=rightmax-arr[right];
+            }
+            right--;
+        }
+       }
+       return water;
+       
+    }
+    
+}
